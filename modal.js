e076144714da468bf7ea64e9aab72e4581335cd1
samaki8 <samira08@gmail.com>
2024-07-02 15:05:48 +0200

    ajout validation email + sauvegarde
    Sauvegarde 02/07

--- modal.js
+++ modal.js
@@ -27,92 +27,25 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-/*
- * Cette fonction prend un nom en paramètre et valide qu'il est au bon format
- * ici : deux caractères au minimum
- * @param {string} first 
- * @return {boolean}
 
-function validerfirst(first) {
-  if (first.length >= 2) {
-    return true
-  }
-  return false
+function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
-/**
- * Cette fonction prend un nom en paramètre et valide qu'il est au bon format
- * ici : deux caractères au minimum
- * @param {string} last 
- * @return {boolean}
- 
-function validerlast(last) {
-  if (last.length >= 2) {
-    console.log("Le nom est valide")
-    return true
-
-  }
-  console.log("Le nom n'est pas valide")
-  return false
-
-}
-/**
-* Cette fonction prend un email en paramètre et valide qu'il est au bon format. 
-* @param {string} email 
-* @return {boolean}
-*/
-/*
-function validerEmail(email) {
-  let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+")
-  if (emailRegExp.test(email)) {
-    return true
-  }
-  return false
-}
-*/
-/*
-* Cette fonction prend la date de naissance en paramètre et valide qu'il est au bon format. 
-* @param {string} birthdate 
-* @return {boolean}
-
-function validerEmail(birthdate) {
-  let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+")
-  if (emailRegExp.test(email)) {
-    return true
-  }
-  return false
-}
-
-function estMajeur(dateNaissance) {
-  var aujourdHui = new Date();
-  var dateNaissance = new Date(dateNaissance);
-
-  var age = aujourdHui.getFullYear() - dateNaissance.getFullYear();
-  var m = aujourdHui.getMonth() - dateNaissance.getMonth();
-
-  if (m < 0 || (m === 0 && aujourdHui.getDate() < dateNaissance.getDate())) {
-    age--;
-  }
-
-  return age >= 18;
-}
-
-*/
-//Show input error message
 
 function showError(input, message) {
   const formData = input.parentElement;
   formData.className = 'formData';
-  const small = formControl.querySelector('[data-error]');
+  const small = formData.querySelector('[data-error]');
   small.innerText = message;
   small.setAttribute('data-error-visible', 'true');
   input.style.border = '2px solid #e54858';
 }
 
-
 function showSuccess(input) {
   const formData = input.parentElement;
   formData.className = 'form-control success';
-  const small = formControl.querySelector('[data-error]');
+  const small = formData.querySelector('[data-error]');
   small.innerText = '';
   small.removeAttribute('data-error-visible');
   input.style.border = '';
@@ -134,9 +67,24 @@ function validate() {
     alert("Le prénom et le nom doivent comporter au moins 2 lettres.");
     showError(document.forms["reserve"]["first"], "Le prénom doit comporter au moins 2 lettres.");
     showError(document.forms["reserve"]["last"], "Le nom doit comporter au moins 2 lettres.");
+    console.log (firstName, lastName);
     return false;
-  }
+    
+   //} else {
+    //showSuccess(document.forms["reserve"]["first"]);
+    //showSuccess(document.forms["reserve"]["last"]);
 
+  }
+  console.log (firstName, lastName);
+  if (!validateEmail(email)) {
+    //alert("Veuillez entrer un email valide.");
+    showError(document.forms["reserve"]["email"], "Veuillez entrer un email valide2.");
+    return false;
+  //} else {
+    //showSuccess (document.forms["reserve"]["email"], "L'émail est valide valide.")
+   
+  }
+  console.log(email);
   var today = new Date();
   var birthDate = new Date(birthdate);
   var age = today.getFullYear() - birthDate.getFullYear();
